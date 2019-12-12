8d889df1ea3ed85ab48d8f82a8f787c52b9c34ce
Duy Tu <duytuhubt@gmail.com>
2019-12-12 13:51:04 +0700

    add repositories

---
+++ app/Repositories/Category/CategoryRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories\Category;
+
+use App\Repositories\EloquentRepository;
+use App\Models\Category;
+
+class CategoryRepository extends EloquentRepository implements CategoryRepositoryInterface
+{
+    public function getModel()
+    {
+        return Category::class;
+    }
+}

359285d741b6224cf0e5ef74cb47c51c0c506a9e
KienTran <kientmc0112@gmail.com>
2020-01-07 16:45:21 +0700

    add calendar fix convention L1

--- app/Repositories/User/UserRepositoryInterface.php
+++ app/Repositories/User/UserRepositoryInterface.php
@@ -4,7 +4,9 @@ namespace App\Repositories\User;
 
 interface UserRepositoryInterface
 {
-    public function getUserByRole($role)
+    public function getUserByRole($role);
 
-    public function findWithCourse($id)
+    public function findWithCourse($id);
+
+    public function findWithTask($id);
 }
